99745c5c8ce9fa3fef76f6600425fc9a3357005e
Apu-kas <viniacosta2020@gmail.com>
2025-09-03 09:11:13 -0300

    commit

--- routes/tarefas_routes.py
+++ routes/tarefas_routes.py
@@ -6,3 +6,8 @@ tarefas_bp = Blueprint('tarefas', __name__)
 @tarefas_bp.route('/tarefas', methods=['GET'])
 def get_listar_tarefas():
     return jsonify([tarefa.to_dict() for tarefa in TarefaController.get_listar_tarefas()])
+
+@tarefas_bp.route('/api/cria_tarefas', methods=['POST'])
+def criar_tarefa():
+    dados = request.get_json()
+    id = dados.get_json()
