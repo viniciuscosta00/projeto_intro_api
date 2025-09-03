b058b6339a435c4985d0d73d498055215fbd182f
Apu-kas <viniacosta2020@gmail.com>
2025-09-03 11:59:35 -0300

    commit

--- config.py
+++ config.py
@@ -1,6 +1,12 @@
 import os
+from urllib.parse import quote_plus
 
 class Config:
     SECRET_KEY = os.getenv("SECRET_KEY', 'wcc@2023")
-    AQLCHEMY_DATABASE_URL = os.getenv("DATABASE_URL" , "postgresql://postgres:SECRET_KEY@127.0.0.1:5432/tarefas3A")
+    password = os.getenv("DATABASE_PASSWORD", "wcc@2023")
+    enconded_password = quote_plus(password)
+    SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL", f"postgresql://postgres:{enconded_password}@127.0.0.1:5432/tarefas3A")
+
+
+    #SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL" , "postgresql://postgres:SECRET_KEY@127.0.0.1:5432/tarefas3A")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
